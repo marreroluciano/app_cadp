0a891b6fc8f6cf84c97c908c87e5f8174189a7b2
marreroluciano <marreroluciano@gmail.com>
2016-07-07 21:02:39 -0300

    alta de solicitud

--- application/models/request_model.php
+++ application/models/request_model.php
@@ -6,10 +6,18 @@
     }
 
     /* obtiene las solicitudes de un usuario */
-    function get_user_requests($id_user){            
+    function get_user_requests($id_user){
+      $this->db->select('request.id, date, start_date_justification, end_date_justification, type_request.detail as type_request_detail, request_state.detail as request_state_detail, id_request_state');
+      $this->db->join('type_request', 'type_request.id = request.id_request_type');
+      $this->db->join('request_state', 'request_state.id = request.id_request_state');
       $this->db->where('user_id',$id_user);
       $query = $this->db->get('request');
       return($query->result());
-    }    
+    }
+
+    /* inserta una nueva solicitud */
+    function insert_request($data){
+      return($this->db->insert('request', $data));
+    }
 }
 ?> 
