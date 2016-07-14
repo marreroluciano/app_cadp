febbcec62f5dd62bacc57238bcf74376fd1f2e16
marreroluciano <marreroluciano@gmail.com>
2016-07-14 12:31:56 -0300

    visualizador de imagenes

--- application/models/turn_model.php
+++ application/models/turn_model.php
@@ -9,6 +9,13 @@
     function get_turns(){      
       $query = $this->db->get('turn');
       return($query->result());
-    }    
+    }
+
+    /* obtiene un turno */
+    function get_turn($id_turn){
+      $this->db->where('id', $id_turn);
+      $query = $this->db->get('turn');
+      return($query->result());
+    }
 }
 ?> 
