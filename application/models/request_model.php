e478e4213164687b627c1c3e2f20c13c68b3a9fb
marreroluciano <marreroluciano@gmail.com>
2016-07-18 08:08:45 -0300

    orden de la solicitudes

--- application/models/request_model.php
+++ application/models/request_model.php
@@ -7,10 +7,11 @@
 
     /* obtiene las solicitudes de un usuario */
     function get_user_requests($id_user){
-      $this->db->select('request.id, date, start_date_justification, end_date_justification, type_request.detail as type_request_detail, request_state.detail as request_state_detail, id_request_state');
+      $this->db->select('request.id, date, start_date_justification, end_date_justification, type_request.detail as type_request_detail, request_state.detail as request_state_detail, request_state.class as request_class, id_request_state');
       $this->db->join('type_request', 'type_request.id = request.id_request_type');
       $this->db->join('request_state', 'request_state.id = request.id_request_state');
       $this->db->where('user_id',$id_user);
+      $this->db->order_by("date", "desc");
       $query = $this->db->get('request');
       return($query->result());
     }
