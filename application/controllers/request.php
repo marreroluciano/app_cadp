378484053b45cccc94fcf5b3dda419858ce4ecca
marreroluciano <marreroluciano@gmail.com>
2016-07-25 09:31:10 -0300

    edicion de un request

--- application/controllers/request.php
+++ application/controllers/request.php
@@ -36,21 +36,22 @@
    }
 
    function insert_request(){
-     if (empty($_POST ) != true) { 
-     //$config['upload_path'] = 'D:\app_cadp\uploads';
-     $config['upload_path'] = 'C:\xampp_v1.8\htdocs\app_cadp\images\uploads';     
-     $config['allowed_types'] = 'gif|jpg|png';
-     //$config['allowed_types'] = '*';
+     if (empty($_POST ) != true) {
+       date_default_timezone_set('America/Argentina/Buenos_Aires');      
+     
+       $config['upload_path'] = 'C:\xampp_v1.8\htdocs\app_cadp\images\uploads';     
+       $config['allowed_types'] = 'gif|jpg|png';
+     
 
-     $this->load->library('upload', $config);
-     $this->upload->initialize($config);
+       $this->load->library('upload', $config);
+       $this->upload->initialize($config);
 
-     $valid_operation = false;
+       $valid_operation = false;
 
-     if (!$this->upload->do_upload("certificate")){
-        $error = $this->upload->display_errors();
-        echo $error;
-     } else { 
+       if (!$this->upload->do_upload("certificate")){
+         $error = $this->upload->display_errors();
+         echo $error;
+       } else { 
          $file_data = $this->upload->data();
          $certificate = $file_data['file_name'];
          $id_request_type = $this->input->post('request_types');
@@ -58,50 +59,49 @@
          switch ($id_request_type) {
            case 1:
              $id_turn = $this->input->post('turn');
-             $value_date_from = null;
-             $value_date_end = null;
+             $date_from = null;
+             $date_end = null;
              break;
            case 2:
-             $date_from = date_create($this->input->post('value_date_from'));
-             $date_end = date_create($this->input->post('value_date_end'));
-             $value_date_from = date_format($date_from, 'Y-m-d');
-             $value_date_end = date_format($date_end, 'Y-m-d');
+             $date_from = str_replace('/', '-', $this->input->post('value_date_from'));
+             $date_end = str_replace('/', '-', $this->input->post('value_date_end'));
+             $date_from = date('Y-m-d', strtotime($date_from));
+             $date_end = date('Y-m-d', strtotime($date_end));
              $id_turn = null;
              break;
-         }
+         }        
          
-         date_default_timezone_set('America/Argentina/Buenos_Aires');
-         $date = date("Y-m-d h:i:s");         
-         $data = array('date' => $date, 
+         $date = date("Y-m-d h:i:s");
+         $data = array('date' => $date,
+                       'date_update' => $date,
                        'current_turn' => null, 
                        'requested_shift' => $id_turn, 
-                       'start_date_justification' => $value_date_from, 
-                       'end_date_justification' => $value_date_end, 
+                       'start_date_justification' => $date_from, 
+                       'end_date_justification' => $date_end, 
                        'reason' => $this->input->post('comments'),
                        'attached' => $certificate,
                        'id_request_type' => $id_request_type,
                        'user_id' => $this->session->userdata['id'],
                        'id_request_state' => 1);
-         $valid_insert = $this->request_model->insert_request($data);
-         //$valid_insert = 1;
+         $valid_insert = $this->request_model->insert_request($data);         
          if ($valid_insert > 0) { $valid_operation = true; }
-     }
+       }
 
-     /* se arma la salida */
-     $output = '';
-     if ($valid_operation) {
-       $output.= '<div class="row">';
-       $output.= '<div class="col-xs-12">';
-       $output.= '<div class="alert alert-success" role="alert"><i class="fa fa-thumbs-up" aria-hidden="true"></i> La solicitud ha sido enviada y en breve ser&aacute; procesada.</div>';
-       $output.= '</div>';
-       $output.= '</div>';
+       /* se arma la salida */
+       $output = '';
+       if ($valid_operation) {
+         $output.= '<div class="row">';
+         $output.= '<div class="col-xs-12">';
+         $output.= '<div class="alert alert-success" role="alert"><i class="fa fa-thumbs-up" aria-hidden="true"></i> La solicitud ha sido enviada y en breve ser&aacute; procesada.</div>';
+         $output.= '</div>';
+         $output.= '</div>';
 
-       $output.= '<div class="row">';
-       $request_type = $this->type_request_model->get_request_type($id_request_type);
-       $output.= '<div class="col-xs-6">';
-       $output.= '<ul class="list-group">';
-       $output.= '<li class="list-group-item"><strong>Tipo de solicitud:</strong> '.$request_type[0]->detail.'</li>';
-       switch ($id_request_type) {
+         $output.= '<div class="row">';
+         $request_type = $this->type_request_model->get_request_type($id_request_type);
+         $output.= '<div class="col-xs-6">';
+         $output.= '<ul class="list-group">';
+         $output.= '<li class="list-group-item"><strong>Tipo de solicitud:</strong> '.$request_type[0]->detail.'</li>';
+         switch ($id_request_type) {
          case 1:
            $turn = $this->turn_model->get_turn($id_turn);
            $output.= '<li class="list-group-item"><strong>Pasar al:</strong> '.$turn[0]->detail.'</li>';
@@ -110,59 +110,58 @@
            $output.= '<li class="list-group-item"><strong>Fecha desde:</strong> '.$this->input->post('value_date_from').'</li>';
            $output.= '<li class="list-group-item"><strong>Fecha hasta:</strong> '.$this->input->post('value_date_end').'</li>';
            break;
+         }
+         $output.= '<li class="list-group-item"><strong>Comentarios:</strong> '.$this->input->post('comments').'</li>';
+         $output.= '</ul>';
+         $output.= '</div>';
+
+         $output.= '<div class="col-xs-6">';
+       
+         /* The Bootstrap Image Gallery lightbox */
+         $output.= '<div id="blueimp-gallery" class="blueimp-gallery">';
+         $output.= '<div class="slides"></div>';
+         $output.= '<h3 class="title"></h3>';
+         $output.= '<a class="prev">‹</a>';
+         $output.= '<a class="next">›</a>';
+         $output.= '<a class="close">×</a>';
+         $output.= '<a class="play-pause"></a>';
+         $output.= '<ol class="indicator"></ol>';
+       
+         $output.= '<div class="modal fade">';
+         $output.= '<div class="modal-dialog">';
+         $output.= '<div class="modal-content">';
+         $output.= '<div class="modal-header">';
+         $output.= '<button type="button" class="close" aria-hidden="true">&times;</button>';
+         $output.= '<h4 class="modal-title"></h4>';
+         $output.= '</div>';
+         $output.= '<div class="modal-body next"></div>';
+
+         $output.= '</div>';
+         $output.= '</div>';
+         $output.= '</div>';
+         $output.= '</div>';
+
+         $output.= '<div id="links">';
+         $output.= '<a href="../images/uploads/'.$certificate.'" title="Certificado" data-gallery>';
+         $output.= '<img src="../images/uploads/'.$certificate.'" alt="Certificado" class="img-rounded img-responsive center-block" width="50%" height="50%" />';
+         $output.= '</a>';
+         $output.= '</div>';
+
+         $output.= '</div>';
+         $output.= '</div>';
+
+         /* BOTON PARA REGRESAR */
+         $output .= '<div class="row">';
+         $output .= '<div class="col-xs-12">';
+         $output .= '<a href="'.base_url().'request" type="button" class="btn btn-success" data-toggle="tooltip" title="Volver al listado"><i class="fa fa-hand-o-left" aria-hidden="true"></i> Volver al listado </a>';
+         $output .= '</div>';
+         $output .= '</div>';
        }
-       $output.= '<li class="list-group-item"><strong>Comentarios:</strong> '.$this->input->post('comments').'</li>';
-       $output.= '</ul>';
-       $output.= '</div>';
-
-       $output.= '<div class="col-xs-6">';
-       
-       /* The Bootstrap Image Gallery lightbox */
-       $output.= '<div id="blueimp-gallery" class="blueimp-gallery">';
-       $output.= '<div class="slides"></div>';
-       $output.= '<h3 class="title"></h3>';
-       $output.= '<a class="prev">‹</a>';
-       $output.= '<a class="next">›</a>';
-       $output.= '<a class="close">×</a>';
-       $output.= '<a class="play-pause"></a>';
-       $output.= '<ol class="indicator"></ol>';
-       
-       $output.= '<div class="modal fade">';
-       $output.= '<div class="modal-dialog">';
-       $output.= '<div class="modal-content">';
-       $output.= '<div class="modal-header">';
-       $output.= '<button type="button" class="close" aria-hidden="true">&times;</button>';
-       $output.= '<h4 class="modal-title"></h4>';
-       $output.= '</div>';
-       $output.= '<div class="modal-body next"></div>';
-
-       $output.= '</div>';
-       $output.= '</div>';
-       $output.= '</div>';
-       $output.= '</div>';
-
-       $output.= '<div id="links">';
-       $output.= '<a href="../images/uploads/'.$certificate.'" title="Certificado" data-gallery>';
-       $output.= '<img src="../images/uploads/'.$certificate.'" alt="Certificado" class="img-rounded img-responsive center-block" width="50%" height="50%" />';
-       $output.= '</a>';
-       $output.= '</div>';
-
-       $output.= '</div>';
-       $output.= '</div>';
-
-       /* BOTON PARA REGRESAR */
-       $output .= '<div class="row">';
-       $output .= '<div class="col-xs-12">';
-       $output .= '<a href="'.base_url().'request" type="button" class="btn btn-success" data-toggle="tooltip" title="Volver al listado"><i class="fa fa-hand-o-left" aria-hidden="true"></i> Volver al listado </a>';
-       $output .= '</div>';
-       $output .= '</div>';
-     }
-     echo $output;
-   } else {redirect('/error_404', 'refresh');}  
+       echo $output;
+     } else {redirect('/error_404', 'refresh');}  
    }
 
-   function view($request_id){
-     
+   function view($request_id = null){     
      $request = $this->request_model->get_request($request_id);
      $view_data['request'] = $request;
 
@@ -170,6 +169,77 @@
      $datos_layout["user_menu"] = $this->load->view('user/menu_view', '', true);
      $datos_layout["content"] = $this->load->view('request/request_view', $view_data, true);
      $this->load->view('layout_view', $datos_layout);
+   }
+
+   function edit($request_id = null){
+     $request = $this->request_model->get_request($request_id);
+     $request_types = $this->type_request_model->get_requests_types();
+     $turns = $this->turn_model->get_turns();
+     
+     $view_data['request_types'] = $request_types;
+     $view_data['turns'] = $turns;
+     $view_data['request'] = $request;
+
+     $datos_layout["title"] = "CADP - Editando solicitud";
+     $datos_layout["user_menu"] = $this->load->view('user/menu_view', '', true);
+     $datos_layout["content"] = $this->load->view('request/edit_request_view', $view_data, true);
+     $this->load->view('layout_view', $datos_layout);
+   }
+
+   function edit_request(){
+     if (empty($_POST ) != true) {
+       $certificate = $this->input->post('certificate');
+
+       $valid_upload = true;
+
+       if ($certificate != '') {
+         $config['upload_path'] = 'C:\xampp_v1.8\htdocs\app_cadp\images\uploads';
+         $config['allowed_types'] = 'gif|jpg|png';
+         $this->load->library('upload', $config);
+         $this->upload->initialize($config);
+
+         if (!$this->upload->do_upload("certificate")){
+           $error = $this->upload->display_errors();           
+           $valid_upload = false;
+           echo $error;
+         } else {
+            $file_data = $this->upload->data();
+            $certificate = $file_data['file_name'];
+         }
+       } else { $certificate = $this->input->post('attached'); }
+
+       /* el upload fue exitoso o no se cambio el archivo que ya existia */
+       if ($valid_upload) {
+         $id_request_type = $this->input->post('request_types');
+         switch ($id_request_type) {
+           case 1:
+             $id_turn = $this->input->post('turn');
+             $value_date_from = null;
+             $value_date_end = null;
+             break;
+           case 2:
+             $date_from = str_replace('/', '-', $this->input->post('value_date_from'));
+             $date_end = str_replace('/', '-', $this->input->post('value_date_end'));
+             $date_from = date('Y-m-d', strtotime($date_from));
+             $date_end = date('Y-m-d', strtotime($date_end));
+             $id_turn = null;
+             break;
+         }        
+
+         date_default_timezone_set('America/Argentina/Buenos_Aires');
+         $date_update = date("Y-m-d h:i:s");
+         $data = array('date_update' => $date_update,                       
+                       'requested_shift' => $id_turn, 
+                       'start_date_justification' => $value_date_from, 
+                       'end_date_justification' => $value_date_end, 
+                       'reason' => $this->input->post('comments'),
+                       'attached' => $certificate
+                       );
+         $valid_update = $this->request_model->update_request($this->input->post('request_id'), $data);
+
+         echo $valid_update;
+       }
+     } else {redirect('/error_404', 'refresh');}
    }
 } 
 ?> 
